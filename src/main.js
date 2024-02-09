25cad109b64330c31f97d327e9709fa10ac1d145
Alenteva <144173680+Alenteva@users.noreply.github.com>
2024-02-09 03:46:53 +0200

    .

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.querySelector('.form');
-const inputSerch = document.querySelector('input');
-const buttonSerch = document.querySelector('button');
+const form = document.querySelector('form');
+const inputSearch = document.querySelector('input');
+const buttonSearch = document.querySelector('button');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
 
@@ -14,32 +14,85 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionPosition: 'bottom',
   captionDelay: 250,
 });
-const URL =
-  'https://pixabay.com/api/?key=42245077d2f8998b656235f8798291f32&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true';
 
-form.addEventListener('sabmit', event => {
-  event.preventDefault();
-  if (inputSerch.value.trim() === '') {
+// function templateImg(images) {
+//   const markup = images
+//     .map(
+//       ({
+//         webformatURL,
+//         largeImageURL,
+//         tags,
+//         likes,
+//         views,
+//         comments,
+//         downloads,
+//       }) =>
+//         ` <li class="gallery-item">
+//         <a class="gallery-link" href="${largeImageURL}">
+//           <img
+//             class="gallery-image"
+//             src="${webformatURL}"
+//             data-source="${largeImageURL}"
+//             width="360px"
+//             height="200px"
+//             alt="${tags}"
+//           />
+//         </a>
+//          <div class="thumb-block">
+//         <div class="block">
+//           <h2 class="title">Likes</h2>
+//           <p class="amount">${likes}</p>
+//         </div>
+//         <div class="block">
+//           <h2 class="title">Views</h2>
+//           <p class="amount">${views}</p>
+//         </div>
+//         <div class="block">
+//           <h2 class="title">Comments</h2>
+//           <p class="amount">${comments}</p>
+//         </div>
+//         <div class="block">
+//           <h2 class="title">Downloads</h2>
+//           <p class="amount">${downloads}</p>
+//         </div>
+//       </div>
+//       </li>`
+//     )
+//     .join('');
+//   gallery.insertAdjacentHTML('beforeend', markup);
+// }
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const search = e.target.elements.search.value;
+  if (search === '') {
     return;
   }
-  fetchImg()
-    .then(images => renderImg(images))
-    .catch(error => console.log(error))
-    .finally(() => {
-      hideLoader();
-    });
+  searchImg().then(data => {
+    renderImg(data.hits);
+  });
+  e.target.reset();
 });
 
-function fetchImg() {
-  return fetch(URL).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+const searchParams = new URLSearchParams({
+  key: '42245077-d2f8998b656235f8798291f32',
+  q: 'cat',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+});
+
+function searchImg() {
+  const BASE_URL = 'https://pixabay.com';
+  const END_POINT = '/api/';
+  const PARAMS = `?${searchParams}`;
+
+  const URL = BASE_URL + END_POINT + PARAMS;
+  return fetch(URL).then(response => response.json());
 }
-function renderImg(images) {
-  const markup = images
+
+function templateImg(images) {
+  return images
     .map(
       ({
         webformatURL,
@@ -50,18 +103,16 @@ function renderImg(images) {
         comments,
         downloads,
       }) =>
-        ` <li class="gallery-item">
-        <a class="gallery-link" href="${largeImageURL}">
-          <img
-            class="gallery-image"
-            src="${webformatURL}"
-            data-source="${largeImageURL}"
-            width="360px"
-            height="200px"
-            alt="${tags}"  
-          />
-        </a>
-         <div class="thumb-block">
+        `<li class="gallery-item">
+      <a class="gallery-link" href="${largeImageURL}">
+        <img
+          class="gallery-image"
+          src="${webformatURL}"
+          alt="${tags}"
+          width="360"
+        />
+      </a>
+      <div class="thumb-block">
         <div class="block">
           <h2 class="title">Likes</h2>
           <p class="amount">${likes}</p>
@@ -79,8 +130,12 @@ function renderImg(images) {
           <p class="amount">${downloads}</p>
         </div>
       </div>
-      </li>`
+    </li>`
     )
     .join('');
+}
+
+function renderImg() {
+  const markup = templateImg(images);
   gallery.insertAdjacentHTML('beforeend', markup);
 }
