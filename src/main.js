e495de66b945be5d08d78a77ca41e88d3005d70f
Alenteva <144173680+Alenteva@users.noreply.github.com>
2024-02-11 04:10:28 +0200

    .

--- src/main.js
+++ src/main.js
@@ -4,91 +4,116 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('form');
-const inputSearch = document.querySelector('input');
-const buttonSearch = document.querySelector('button');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
 
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
-});
+let search;
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  const search = e.target.elements.search.value;
+  search = e.target.elements.search.value;
   if (search === '') {
     return;
   }
-  searchImg().then(data => {
-    renderImg(data.hits);
-  });
+  gallery.innerHTML = '';
+  showLoader();
+  getSearchImg(search)
+    .then(data => {
+      renderImg(data.hits);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+    .finally(() => {
+      hideLoader();
+    });
   e.target.reset();
 });
 
-const searchParams = new URLSearchParams({
-  key: '42245077-d2f8998b656235f8798291f32',
-  q: 'cat',
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: 'true',
-});
-
-function searchImg() {
+function getSearchImg(search) {
+  const searchParams = new URLSearchParams({
+    key: '42245077-d2f8998b656235f8798291f32',
+    q: `${search}`,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
   const BASE_URL = 'https://pixabay.com';
   const END_POINT = '/api/';
   const PARAMS = `?${searchParams}`;
-
   const URL = BASE_URL + END_POINT + PARAMS;
-  return fetch(URL).then(response => response.json());
+
+  return fetch(URL).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
 
 function templateImg(images) {
   return images
     .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) =>
+      item =>
         `<li class="gallery-item">
-      <a class="gallery-link" href="${largeImageURL}">
+      <a class="gallery-link" href="${item.largeImageURL}">
         <img
           class="gallery-image"
-          src="${webformatURL}"
-          alt="${tags}"
+          src="${item.webformatURL}"
+          alt="${item.tags}"
           width="360"
         />
       </a>
-      <div class="thumb-block"
-        <div class="block">
+      <ul class="thumb-block">
+        <li class="block">
           <h2 class="title">Likes</h2>
-          <p class="amount">${likes}</p>
-        </div>
-        <div class="block">
+          <p class="amount">${item.likes}</p>
+        </li>
+        <li class="block">
           <h2 class="title">Views</h2>
-          <p class="amount">${views}</p>
-        </div>
-        <div class="block">
+          <p class="amount">${item.views}</p>
+        </li>
+        <li class="block">
           <h2 class="title">Comments</h2>
-          <p class="amount">${comments}</p>
-        </div>
-        <div class="block">
+          <p class="amount">${item.comments}</p>
+        </li>
+        <li class="block">
           <h2 class="title">Downloads</h2>
-          <p class="amount">${downloads}</p>
-        </div>
-      </div>
+          <p class="amount">${item.downloads}</p>
+        </li>
+      </ul>
     </li>`
     )
     .join('');
 }
 
 function renderImg(images) {
+  if (images.length === 0) {
+    iziToast.error({
+      title: 'Error',
+      message:
+        'Sorry, there are no images matching your search query. Please try again.',
+      position: 'topCenter',
+    });
+    return;
+  }
   const markup = templateImg(images);
   gallery.insertAdjacentHTML('beforeend', markup);
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+  lightbox.refresh();
+}
+function showLoader() {
+  if (loader) {
+    loader.style.display = 'block';
+  }
+}
+
+function hideLoader() {
+  if (loader) {
+    loader.style.display = 'none';
+  }
 }
